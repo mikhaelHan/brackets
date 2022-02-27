e94c499a268ad255d57c868d079a51df9f832acc
mikhaelHan <hancharuk270185@mail.ru>
2022-02-27 21:11:05 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let flag = true;
+  const stack = [];
+  str.split('').forEach(strElement => {
+    bracketsConfig.forEach(configElement => {  ///  |(
+      if (strElement === configElement[0] && strElement === configElement[1]) {
+        if (stack.length === 0) { stack.push(strElement) }
+        else if (stack[stack.length - 1] !== configElement[0] && !stack.includes(strElement)) { stack.push(strElement) }
+        else if (stack[stack.length - 1] === configElement[0]) {
+          stack.pop();
+        }
+        else flag = false
+      }
+      else if (strElement === configElement[0] && strElement !== configElement[1]) {
+        stack.push(strElement);
+      }
+      else if (strElement === configElement[1] && strElement !== configElement[0]) {
+        if (stack.length !== 0 && stack[stack.length - 1] === configElement[0]) {
+          stack.pop();
+        }
+        else flag = false;
+      }
+    })
+  });
+  if (flag === true && stack.length === 0) return true;
+  else return false
 }
